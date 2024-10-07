1a13129b940b06f5d4ad866bd6e6c4236407b0e9
lmcmz <lmcmze@gmail.com>
2024-10-07 12:25:24 +1100

    Update signTypeData.tsx

--- component/signTypeData.tsx
+++ component/signTypeData.tsx
@@ -2,10 +2,23 @@
 import { useSignMessage, useAccount, useSignTypedData } from 'wagmi';
 import { Button } from '@nextui-org/react';
 import { useEffect, useState } from 'react';
-import { verifyMessage, getBytecode } from '@wagmi/core'
+import { verifyTypedData } from '@wagmi/core'
 import { config } from './config'
 
 export function SignTypeData() {
+
+  const types = {
+    Person: [
+      { name: 'name', type: 'string' },
+      { name: 'wallet', type: 'address' },
+    ],
+    Mail: [
+      { name: 'from', type: 'Person' },
+      { name: 'to', type: 'Person' },
+      { name: 'contents', type: 'string' },
+    ],
+  }
+
   const message = {
     types: {
       Person: [
@@ -32,19 +45,44 @@ export function SignTypeData() {
     }
   }
 
-  const { signTypedDataAsync } = useSignTypedData()
+  const domain = {
+    name: 'Ether Mail',
+    version: '1',
+    chainId: 747,
+    verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+  } as const
+
+  const [ isVaild, setVaild ] = useState<boolean>(false);
+  // const [ sig, setSig ] = useState<`0x${string}`>('0x');
+  const { signTypedDataAsync } = useSignTypedData({config})
+
+  const { address } = useAccount()
 
   const signMsg = async () => {
-     // @ts-ignore: Unreachable code error
+    //  @ts-ignore: Unreachable code error
     const result = await signTypedDataAsync(message)
-    // const isVaild = await verifyMessage(config, {
-    //   address: address ?? "0x",
-    //   message: message,
-    //   signature: result,
-    // })
+
+    const isVaild = await verifyTypedData(config, {
+      domain,
+      types,
+      message: {
+        from: {
+          name: 'Cow',
+          wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+        },
+        to: {
+          name: 'Bob',
+          wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+        },
+        contents: 'Hello, Bob!',
+      },
+      primaryType: 'Mail',
+      address: address as `0x${string}`,
+      signature: result
+    })
     // setVaild(isVaild);
     console.log('result ==>', result)
-    // console.log('isVaild ==>', isVaild)
+    console.log('isVaild ==>', isVaild)
   }
 
   return (
@@ -52,6 +90,7 @@ export function SignTypeData() {
       <Button onClick={signMsg} >
         Sign Typed Data
       </Button>
+      <div>{`isValid: ${isVaild}` }</div>
       </div>
   );
 }
